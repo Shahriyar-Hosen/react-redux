a4326187130e37246b5b3304307b458b7db0ba1c
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-08-16 09:19:09 +0600

    App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Counter from "./components/Counter";
+import Stats from "./components/Stats";
 
-function App() {
+const initialState = [
+  {
+    id: 1,
+    count: 0,
+  },
+  {
+    id: 2,
+    count: 0,
+  },
+];
+
+const App = () => {
+  const [state, setState] = useState(initialState);
+
+  const totalCount = () => {
+    return state.reduce((total, counter) => total + counter.count, 0);
+  };
+
+  const increment = (id) => {
+    const updatedCounter = state.map((c) => {
+      if (c.id === id) {
+        return {
+          ...c,
+          count: c.count + 1,
+        };
+      }
+      return { ...c };
+    });
+    setState(updatedCounter);
+  };
+
+  const decrement = (id) => {
+    const updatedCounter = state.map((c) => {
+      if (c.id === id) {
+        return {
+          ...c,
+          count: c.count - 1,
+        };
+      }
+      return { ...c };
+    });
+    setState(updatedCounter);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-screen h-screen p-10 bg-gray-100 text-slate-700">
+      <h1 className="max-w-md mx-auto text-center text-2xl font-bold">
+        Simple Counter Application
+      </h1>
+
+      <div className="max-w-md mx-auto mt-10 space-y-5">
+        {state.map((counter) => (
+          <Counter
+            key={counter.id}
+            id={counter.id}
+            count={counter.count}
+            increment={increment}
+            decrement={decrement}
+          />
+        ))}
+        <Stats count={totalCount()} />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
